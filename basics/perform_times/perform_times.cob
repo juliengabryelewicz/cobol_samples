426caf91026b64f2bcd2dadb612c7bef0ae590b3
Julien Gabryelewicz <julien.gabryelewicz2@gmail.com>
2024-08-14 15:37:55 +0200

    Add perform_times

---
+++ basics/perform_times/perform_times.cob
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PERFORM-TIMES.
+       AUTHOR. Julien Gabryelewicz.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           PERFORM 5 TIMES
+               DISPLAY "Hello, world!"
+           END-PERFORM
+           STOP RUN.
